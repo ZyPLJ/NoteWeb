1bd2d2dfcaa10f8d76d63f0a25875fbcf9b0d106
ZyPLJ <1767992919@qq.com>
2025-11-13 22:10:47 +0800

    feat: 定时任务仪表板添加ip地址日志

--- NoteWeb/Expand/HangfireDashboardAuthorizationFilter.cs
+++ NoteWeb/Expand/HangfireDashboardAuthorizationFilter.cs
@@ -6,10 +6,13 @@ namespace NoteWeb.Expand;
 public class HangfireDashboardAuthorizationFilter : IDashboardAuthorizationFilter
 {
     private readonly IConfiguration _configuration;
+    private readonly ILogger<HangfireDashboardAuthorizationFilter> _logger;
 
-    public HangfireDashboardAuthorizationFilter(IConfiguration configuration)
+    public HangfireDashboardAuthorizationFilter(IConfiguration configuration,
+        ILogger<HangfireDashboardAuthorizationFilter> logger)
     {
         _configuration = configuration;
+        _logger = logger;
     }
 
     public bool Authorize(DashboardContext context)
@@ -24,6 +27,8 @@ public class HangfireDashboardAuthorizationFilter : IDashboardAuthorizationFilte
 
         // 检查是否在白名单IP中
         var clientIp = httpContext.Connection.RemoteIpAddress?.ToString();
+        _logger.LogInformation("Client IP: {0}", clientIp);
+
         var allowedIps = _configuration.GetSection("Hangfire:AllowedIPs").Get<string[]>() ?? Array.Empty<string>();
 
         if (allowedIps.Contains(clientIp))
